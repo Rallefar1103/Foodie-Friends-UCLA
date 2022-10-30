15391aa16719545b4d20df145de47c5cb4a0052b
Rallefar1103 <rasmus@turftank.com>
2022-10-30 11:11:06 -0700

    adds header and placeholder image

--- frontend/src/screens/settings/SettingsScreen.js
+++ frontend/src/screens/settings/SettingsScreen.js
@@ -1,20 +1,47 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const SettingsScreen = () => {
   return (
-    <View style={styles.screen}>
-      <Text> Hello from the settings screen </Text>
+    <View style={styles.screenView}>
+      <Text style={styles.headerText}> Settings </Text>
+
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../../../assets/images/gene.png")}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  screenView: {
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 15,
     alignItems: "center",
+    backgroundColor: "#F5F5F5",
+  },
+
+  headerText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "orange",
+  },
+
+  imageContainer: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+    marginBottom: 20,
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
   },
 });
 
