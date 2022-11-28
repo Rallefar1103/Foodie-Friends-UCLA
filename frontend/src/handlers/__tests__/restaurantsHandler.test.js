786db6e8001b7270e456edba27eda969639fe492
Rallefar1103 <rasmus@turftank.com>
2022-11-27 18:10:26 -0800

    adds a test for restaurantsHandler getRestaurantsMethod

--- frontend/src/handlers/__tests__/restaurantsHandler.test.js
+++ frontend/src/handlers/__tests__/restaurantsHandler.test.js
@@ -1,13 +1,48 @@
-import RestaurantsHandler from "../restaurantsHandler";
-import { describe, expect, jest, test } from "@jest/globals";
-import DBHandler from "../dbHandler";
+import { beforeEach, describe, expect, jest, test } from "@jest/globals";
+import { getRestaurantsForDisplay } from "../restaurantsHandler";
+
+var list = [
+  {
+    id: 123,
+    categories: ["asian", "american", "chinese"],
+    distance: 23,
+    location: "Westwood",
+    name: "chick-fil-a",
+    price: "expensive",
+    rating: 4,
+    image_url: "http://here.com",
+  },
+  {
+    id: 124,
+    categories: ["asian", "american", "chinese"],
+    distance: 21,
+    location: "Westwood",
+    name: "chick-fil-a",
+    price: "expensive",
+    rating: 4,
+    image_url: "http://here.com",
+  },
+  {
+    id: 125,
+    categories: ["asian", "american", "chinese"],
+    distance: 26,
+    location: "Westwood",
+    name: "chick-fil-a",
+    price: "expensive",
+    rating: 4,
+    image_url: "http://here.com",
+  },
+];
+
+jest.mock("../restaurantsHandler", () => ({
+  __esModule: true,
+  getRestaurantsForDisplay: () => list,
+}));
 
 describe("restaurantsHandler module", () => {
-  test("should return a list of restaurants from DB", () => {
-    var mock = jest.mock(DBHandler);
+  test("should return a list of restaurants from DB", async () => {
+    var result = getRestaurantsForDisplay();
 
-    var resHandler = RestaurantsHandler();
-    var results = resHandler.getRestaurantsForDisplay(93065);
-    expect(true);
+    expect(result).toBe(list);
   });
 });
